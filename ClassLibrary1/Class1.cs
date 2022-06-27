699510890ad4b997b9c8d0e77589ba9c0b24d777
NikinKoodit <niki.miettinen@awacademy.training>
2022-06-27 15:42:53 +0300

    lisää metodeja

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,53 +4,58 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        // testi    
+        public bool Totuus { get; set; }
+        public DateTime StartLearninDate { get; set; }
+        public bool InProgress { get; set; }
+        public DateTime CompletionDate { get; set; }
 
-            public bool Totuus { get; set; }
-            public DateTime StartLearninDate { get; set; }
-            public bool InProgress { get; set; }
-            public DateTime CompletionDate { get; set; }
-
-            public string Paivat()
+        public string Kiire()
+        {
+            return $"Olet myöhässä!{(CompletionDate - StartLearninDate).TotalDays}";
+        }
+        public string Aikataulussa()
+        {
+            return $"Sinulla on vielä {(CompletionDate - StartLearninDate).TotalDays} päivää aikaa";
+        }
+        public void Tarkistus()
+        {
+            if (CompletionDate > DateTime.Today)
             {
-                return $"Olet myöhässä!{(CompletionDate - StartLearninDate).TotalDays}";
-            }
-
-            public void Tarkistus()
-            {
-                if (CompletionDate > DateTime.Today)
-                {
-                    Totuus = true;
-
-                }
-
-                else
-                {
-                    Totuus = false;
-                }
+                Totuus = true;
 
             }
 
-            public void Myohassa()
+            else
             {
-                if (InProgress == false)
-                {
-                    if (CompletionDate <= DateTime.Today)
-                    {
-                        TimeSpan i = new TimeSpan();
-                        Console.WriteLine(Paivat());
-
-                    }
-                }
-
-                else
-                {
-                    Console.WriteLine("Opiskelusi on aikataulussa.");
-                }
-
+                Totuus = false;
             }
-
 
         }
+
+        public void Myohassa()
+        {
+            if (InProgress == false)
+            {
+                if (CompletionDate <= DateTime.Today)
+                {
+
+                    Console.WriteLine(Kiire());
+
+                }
+                if (CompletionDate > DateTime.Today)
+                {
+                    Console.WriteLine(Aikataulussa());
+                }
+            }
+
+            if (InProgress == true)
+            {
+                Console.WriteLine($"Opiskelusi kesti:{(CompletionDate - StartLearninDate).TotalDays} ");
+            }
+
+        }
+
+
     }
-}
 }
