0b30a190cac1a6c01bd9c28c73b6cf6aab6dcdd8
NikinKoodit <niki.miettinen@awacademy.training>
2022-06-27 14:33:51 +0300

    restart

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,5 +4,53 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+
+            public bool Totuus { get; set; }
+            public DateTime StartLearninDate { get; set; }
+            public bool InProgress { get; set; }
+            public DateTime CompletionDate { get; set; }
+
+            public string Paivat()
+            {
+                return $"Olet myöhässä!{(CompletionDate - StartLearninDate).TotalDays}";
+            }
+
+            public void Tarkistus()
+            {
+                if (CompletionDate > DateTime.Today)
+                {
+                    Totuus = true;
+
+                }
+
+                else
+                {
+                    Totuus = false;
+                }
+
+            }
+
+            public void Myohassa()
+            {
+                if (InProgress == false)
+                {
+                    if (CompletionDate <= DateTime.Today)
+                    {
+                        TimeSpan i = new TimeSpan();
+                        Console.WriteLine(Paivat());
+
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Opiskelusi on aikataulussa.");
+                }
+
+            }
+
+
+        }
     }
+}
 }
